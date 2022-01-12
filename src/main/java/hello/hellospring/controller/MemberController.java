1fea8d04a6c30f9062fd6af609c8b23d3bea301e
xiaogege <sch12635@gmail.com>
2022-01-12 20:25:40 +0900

    [회원가입] 간단한 회원가입 만들기 - MemberService 작성중 22.01.12.

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,10 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService = new MemberController();
+
+
+}
