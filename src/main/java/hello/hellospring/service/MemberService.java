6dc236f2057b3082126c264ef1b8b3044836ece8
xiaogege <sch12635@gmail.com>
2022-01-25 22:02:06 +0900

    [회원가입] 간단한 회원가입 만들기 - 비지니스로직 22.01.25.

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,6 +5,7 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -21,13 +22,24 @@ public class MemberService {
     }
 
     private void vaildateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName()).isPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다!!!");
-        });
+        // ifPresent -> null이 아니라 값이 있으면 동작
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다!!!");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
     }
 
 
-
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 
 }
 
